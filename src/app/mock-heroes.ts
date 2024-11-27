6070fc58f4e480c2cbb0b8c236d7c2fbc7d27f18
DanyCrk <danielfsanchez30@gmail.com>
2024-11-27 11:05:33 -0500

    Se agrega el proyecto

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import { Hero } from "./hero";
+export const HEROES: Hero[]=[
+    { id: 2, name: 'Superman' },
+    { id: 3, name: 'Batman' },
+    { id: 5, name: 'Goku' },
+    { id: 5, name: 'Vegeta' },
+    { id: 6, name: 'Cell' },
+    { id: 7, name: 'Frezzer' },
+    { id: 8, name: 'Jiren' },
+    { id: 9, name: 'Gohan' },
+    { id: 20, name: 'Goten' }
+]
